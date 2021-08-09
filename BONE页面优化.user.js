1f5a7cd08e95f51d6e83951a6d54fa30d775ffa2
Yesaye <46039670+Yesaye@users.noreply.github.com>
2021-08-09 11:12:20 +0800

    Update BONE页面优化.user.js

--- BONE页面优化.user.js
+++ BONE页面优化.user.js
@@ -4,18 +4,21 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       Yesaye
-// @match        http://bone.banksteeltech.com/change/detail?id=*
+// @match        http://bone.banksteeltech.com/*
 // @icon         https://www.google.com/s2/favicons?domain=banksteeltech.com
 // @grant        none
 // ==/UserScript==
 
 (function() {
-
+    var i = 0;
     var t = setInterval(()=>{
         var p = document.getElementsByClassName("ivu-drawer-right")[0];
-        console.log("1");
+        console.log(i++);
         if (p) {
             p.style.width= "1450px";
+            clearInterval(t);
+        }
+        if (i>50) {
             clearInterval(t);
         }
     },500);
