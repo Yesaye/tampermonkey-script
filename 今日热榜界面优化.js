adf667c5796b0cf16550b88afac3a2b2430b86de
Yesaye <46039670+Yesaye@users.noreply.github.com>
2021-08-27 09:42:50 +0800

    Update 今日热榜界面优化.js

--- 今日热榜界面优化.js
+++ 今日热榜界面优化.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         今日热榜界面简化
 // @namespace    http://tampermonkey.net/
-// @version      2.2.3
+// @version      2.2.4
 // @description  仅适用于未登录状态的主界面 自定义背景颜色 卡片颜色 卡片圆角 修改了图标和标题
 // @author       Yesaye
 // @match        *://tophub.today/
