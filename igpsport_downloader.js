f30f407e31ffe5d65291cc19146c6af9eee1bdfc
yesaye <yesaye142857@outlook.com>
2025-05-18 01:24:54 +0800

    修复脚本作者信息，更新为正确的作者名称

--- igpsport_downloader.js
+++ igpsport_downloader.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.0.0
 // @description  iGPSPORT的运动记录fit文件的增加批量下载功能
-// @author       script
+// @author       Yesaye
 // @match        https://app.zh.igpsport.com/sport/history/list
 // @grant        GM_addStyle
 // @grant        unsafeWindow
